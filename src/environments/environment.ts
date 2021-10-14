2df580c71fc39a2508fca64bf37c6f5d16aba3e2
andrasat <andra.satria1@gmail.com>
2021-10-14 13:10:48 +0700

    update env values for qs service

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,11 +4,9 @@
 
 export const environment = {
   production: false,
-  apiFS: 'https://dev8000.esb.co.id/eso-api',
-  bearerFS: '1',
   transID: '58280eb4ca0e7ef22b08814b8016006666626132353733313636393137666231626130383536623130386237633666336336353935666239396133353066366564353364633734653631383861343431637a3f774720a5b0bd49d588bd6635035ab606841bd2d92e8959a79bc0c880e89a01bfe154c6ff5085f36de922f11512',
-  apiQS: 'https://dev7.esb.co.id/api-ezo-dev',
-  bearerQS: 'zmxncbv1029384756',
+  apiQS: 'https://stg7.esb.co.id/api-ezo-int',
+  bearerQS: 'cjNPJgQJrdtlP3qgnO5f6qZ7lZML9n2XkoynmVPqSB1QseIxwImTDGBEVQPJ',
   firebase: {},
 };
 
