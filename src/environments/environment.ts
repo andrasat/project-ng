642a0f5cf74eda84d5b097b26690c83d1d00767d
andrasat <andra.satria1@gmail.com>
2021-11-03 18:04:20 +0700

    update env and build command

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,12 +6,13 @@ export const environment = {
   production: false,
   // QS API
   transID: '58280eb4ca0e7ef22b08814b8016006666626132353733313636393137666231626130383536623130386237633666336336353935666239396133353066366564353364633734653631383861343431637a3f774720a5b0bd49d588bd6635035ab606841bd2d92e8959a79bc0c880e89a01bfe154c6ff5085f36de922f11512',
-  apiQS: 'https://stg7.esb.co.id/api-ezo-int',
-  devApiQS: 'https://dev7.esb.co.id/api-ezo-dev',
-  bearerQS: 'cjNPJgQJrdtlP3qgnO5f6qZ7lZML9n2XkoynmVPqSB1QseIxwImTDGBEVQPJ',
-  devBearerQS: 'zmxncbv1029384756',
+  apiQS: 'https://dev7.esb.co.id/api-ezo-dev',
+  bearerQS: 'zmxncbv1029384756',
   usernameQS: 'ESBADMIN',
   passwordQS: '14ecdbacbc51885a09c4d0592e685898',
+  // TEST
+  _apiQS: 'https://stg7.esb.co.id/api-ezo-int',
+  _bearerQS: 'cjNPJgQJrdtlP3qgnO5f6qZ7lZML9n2XkoynmVPqSB1QseIxwImTDGBEVQPJ',
   // OTHER
   mapboxToken: 'pk.eyJ1IjoiYW5kcmFzYXQiLCJhIjoiY2t1d2JvMzUyMjllODJvczdtbnBub2RqcyJ9.DZdpSQCcqxoopSpQcaZIpw',
   firebase: {
